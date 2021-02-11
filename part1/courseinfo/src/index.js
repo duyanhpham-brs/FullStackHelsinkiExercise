edc45ca5d3d099aa44538baa611234e4377f2357
duyanhpham-brs <duy.pham@smail.inf.h-brs.de>
2021-02-11 16:08:18 +0100

    submit part1 courseinfo step 1 (exercise 1.1)

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <p>{props.part} {props.no_exercises}</p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.total_exercises}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content part={part1} no_exercises={exercises1}/>
+      <Content part={part2} no_exercises={exercises2}/>
+      <Content part={part3} no_exercises={exercises3}/>
+      <Total total_exercises={exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
