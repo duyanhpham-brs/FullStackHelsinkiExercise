2804eefa10115c169545843541d29363c2fdbe8c
duyanhpham-brs <duy.pham@smail.inf.h-brs.de>
2021-02-11 16:36:14 +0100

    submit part1 courseinfo step 2 (exercise 1.2)

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -7,9 +7,19 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <p>{props.part} {props.no_exercises}</p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.part1} no_exercises={props.exercises1}/>
+      <Part part={props.part2} no_exercises={props.exercises2}/>
+      <Part part={props.part3} no_exercises={props.exercises3}/>
+    </>
   )
 }
 
@@ -31,9 +41,7 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content part={part1} no_exercises={exercises1}/>
-      <Content part={part2} no_exercises={exercises2}/>
-      <Content part={part3} no_exercises={exercises3}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
       <Total total_exercises={exercises1 + exercises2 + exercises3}/>
     </div>
   )
